1e65d95b337917853e1ad2efe3d4527af1b4be8c
Lewis Potticary <lewispotticary@gmail.com>
2021-12-05 20:38:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+import RandomLocations from "./RandomLocations.js";
+
+
+    const arrayLength = RandomLocations.RandomLocations.length - 1;
+    const startIndex = Math.floor(Math.random() * arrayLength);
+    const locationLat = RandomLocations.RandomLocations[startIndex][0];
+    const locationLng = RandomLocations.RandomLocations[startIndex][1];
+    const streetviewLocation = { lat: locationLat, lng: locationLng};
+    
+
+    //Map Function
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 32.397, lng: -25.644 },
+        zoom: 2,
+    });
+
+    //Street View Function
+    const panorama = new google.maps.StreetViewPanorama(
+      document.getElementById("pano"),
+      {
+        position: streetviewLocation,
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
+      }
+    );
+
+    //Place Marker Function
+
+    var marker;
+
+    function placeMarker(location) {
+        if ( marker ) {
+          marker.setPosition(location);
+        } else {
+          marker = new google.maps.Marker({
+            position: location,
+            map: map
+          });
+        }
+    }
+
+    
+    function nextRound(){
+        const randomIndex = Math.floor(Math.random() * arrayLength);
+        const locationLat = RandomLocations.RandomLocations[randomIndex][0];
+        const locationLng = RandomLocations.RandomLocations[randomIndex][1];
+        const streetviewLocation = { lat: locationLat, lng: locationLng};
+        panorama.setPosition(streetviewLocation);
+    }
+
+    //Event Listeners
+      
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    }); 
+
+    document.getElementById("next-button").addEventListener("click", nextRound);
+
+
+
